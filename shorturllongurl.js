b2340edc069b787e87e6f8a8b0527f234ead1140
Nate Sawant <74106957+natesawant@users.noreply.github.com>
2021-10-06 14:37:18 -0400

    Add Javascript file

---
+++ shorturllongurl.js
@@ -0,0 +1,15 @@
+function getlongurl(surl) {
+    const json_data = require('./data.json');
+
+    for (i = 0; i < json_data.data.length; i++) {
+        s = json_data.data[i].shorturl;
+        if (s === surl) {
+            l = json_data.data[i].longurl;
+            return l;
+        }
+    }
+}
+
+console.log(getlongurl('TEST1'));
+
+//location.replace(getlongurl(window.location.pathname));
