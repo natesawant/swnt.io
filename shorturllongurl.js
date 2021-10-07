f085de43ed3ab86c4e8ed401d38d6171c2d76852
Nate Sawant <74106957+natesawant@users.noreply.github.com>
2021-10-07 13:44:22 -0400

    Update shorturllongurl.js

--- shorturllongurl.js
+++ shorturllongurl.js
@@ -1,5 +1,6 @@
 function getlongurl(surl) {
-    const json_data = require('./data.json');
+    //const json_data = require('./data.json');
+    const json_data = fetch('./data.json');
 
     for (i = 0; i < json_data.data.length; i++) {
         s = json_data.data[i].shorturl;
